60885281a7e72b4acdeeee0626f7b45aa49128fb
Andreas Trefzer <atr@mm-software.com>
2024-04-27 15:32:58 +0200

    Remvoe dead code

--- Launcher/Profiler/Process.cs
+++ Launcher/Profiler/Process.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Reflection;
-using System.Reflection.PortableExecutable;
 using System.Threading.Tasks;
 
 namespace Launcher.Profiler
@@ -30,7 +27,7 @@ namespace Launcher.Profiler
             Environment.SetEnvironmentVariable("MINI_PROFILER_OUT_DIR", outputDirectory);
             Environment.SetEnvironmentVariable("COR_PROFILER", "{7E981B79-6303-483F-A372-8169B1073A0F}");
             Environment.SetEnvironmentVariable("COR_ENABLE_PROFILING", "1");
-            // The COM object is not registered. Instead it is sufficient to pass the file path to the profiler dll.
+            // The COM object is not registered. Instead, it is sufficient to pass the file path to the profiler dll.
             Environment.SetEnvironmentVariable("COR_PROFILER_PATH", profilerDll);
 
             // .NET6 and above
@@ -41,38 +38,11 @@ namespace Launcher.Profiler
 
 
             // Start child process and inherit environment variables
-
             return Task.Run(() =>
                             {
                                 var process = System.Diagnostics.Process.Start(target);
                                 process?.WaitForExit();
                             });
-        }
-
-
-        /// <summary>
-        /// Does not work with .net 6 native exe wrappers.
-        /// </summary>
-        private static bool Is64Bit_old(string path)
-        {
-            // Launcher is compiled x64 always.
-
-            var assembly = Assembly.LoadFile(Path.GetFullPath(path));
-            var manifestModule = assembly.ManifestModule;
-            PortableExecutableKinds peKind;
-            manifestModule.GetPEKind(out peKind, out var machine);
-
-            return machine == ImageFileMachine.AMD64;
-        }
-
-        static bool Is64Bit(string filePath)
-        {
-            using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-            using (PEReader reader = new PEReader(stream))
-            {
-                var readerResult = reader.PEHeaders.PEHeader.Magic;
-                return readerResult == PEMagic.PE32Plus;
-            }
         }
     }
 }
